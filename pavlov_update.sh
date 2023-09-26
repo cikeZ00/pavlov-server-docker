d4ff9007ad0b53953edc8144f97738eec55a8a6c
cikeZ00 <17727672+cikeZ00@users.noreply.github.com>
2023-09-26 03:29:19 +0200

    Update pavlov_update.sh

--- pavlov_update.sh
+++ pavlov_update.sh
@@ -2,9 +2,12 @@
 
 echo -e "Beginning Pavlov VR update run on $(date)\n\n"
 
-/home/steam/Steam/steamcmd.sh +force_install_dir "/home/steam/pavlovserver" +login anonymous +app_update 622970 +exit
+/home/steam/Steam/steamcmd.sh +force_install_dir /home/steam/pavlovserver +login anonymous +app_update 622970 -beta default +exit
 /home/steam/Steam/steamcmd.sh +login anonymous +app_update 1007 +quit
 cp "/home/steam/Steam/steamapps/common/Steamworks SDK Redist/linux64/steamclient.so" "/home/steam/.steam/sdk64/steamclient.so"
 cp "/home/steam/Steam/steamapps/common/Steamworks SDK Redist/linux64/steamclient.so" "/home/steam/pavlovserver/Pavlov/Binaries/Linux/steamclient.so"
+sudo rm /usr/lib/x86_64-linux-gnu/libc++.so
+sudo ln -s /usr/lib/x86_64-linux-gnu/libc++.so.1 /usr/lib/x86_64-linux-gnu/libc++.so
+chmod +x ~/pavlovserver/PavlovServer.sh
 
 echo -e "Ending Pavlov VR update run on $(date)\n\n"
